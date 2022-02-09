f11112504a2d45d66d8a06aa8a4ae3f0c42f8e32
Daniel <22991229+SentientCoffee@users.noreply.github.com>
2022-02-08 20:24:59 -0500

    [Add] Compile and run changes for lab02 part 1

---
+++ lab02/src/main/java/csci2020u/lab02/SimpleTest.java
@@ -0,0 +1,9 @@
+package csci2020u.lab02;
+
+class App {
+    public static void main(String[] args) {
+        System.out.println("===========");
+        System.out.println("Hello world");
+        System.out.println("===========");
+    }
+}
